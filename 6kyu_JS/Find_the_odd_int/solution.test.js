4a300db72c6c31a781c9fac2f963ea17924e9373
narae <narae1096@gmail.com>
2023-02-28 14:41:38 +0000

    last test

--- 6kyu_JS/Find_the_odd_int/solution.test.js
+++ 6kyu_JS/Find_the_odd_int/solution.test.js
@@ -16,4 +16,8 @@ describe("findOdd", () => {
   test("should return 0", () => {
     expect(findOdd([0,1,0,1,0])).toBe(0);
   });
+
+  test("should return 4", () => {
+    expect(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1])).toBe(4);
+  });
 });
