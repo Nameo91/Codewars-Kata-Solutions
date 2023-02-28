5d4cd30b9569af7c84d456c446c862f8b0a9fdf5
narae <narae1096@gmail.com>
2023-02-28 14:41:27 +0000

    test with more numbers

--- 6kyu_JS/Find_the_odd_int/solution.js
+++ 6kyu_JS/Find_the_odd_int/solution.js
@@ -1,5 +1,22 @@
 findOdd = (numbers) => {
-  return numbers[0];
+  let occurences = {};
+  let oddInt;
+  
+  for (let i=0; i < numbers.length; i++) {
+    if (occurences[numbers[i]]) {
+      occurences[numbers[i]] += 1 
+    } else {
+      occurences[numbers[i]] = 1   
+    }
+  }
+  
+  for (let key in occurences) {
+    if (occurences[key] % 2 === 1) {
+      oddInt = Number(key); 
+    }
+  }
+
+  return oddInt;
 };
 
 module.exports = findOdd;
--- 6kyu_JS/Find_the_odd_int/solution.test.js
+++ 6kyu_JS/Find_the_odd_int/solution.test.js
@@ -8,4 +8,12 @@ describe("findOdd", () => {
   test("should return 0", () => {
     expect(findOdd([0])).toBe(0);
   });
+
+  test("should return 2", () => {
+    expect(findOdd([1,1,2])).toBe(2);
+  });
+
+  test("should return 0", () => {
+    expect(findOdd([0,1,0,1,0])).toBe(0);
+  });
 });
