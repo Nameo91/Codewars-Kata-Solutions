34a8d7433c53dd4151fba9f7197b66e560ddc5a4
narae <narae1096@gmail.com>
2023-03-02 17:06:42 +0000

    take out an empty array

--- 6kyu_JS/Array.diff/solution.test.js
+++ 6kyu_JS/Array.diff/solution.test.js
@@ -8,4 +8,8 @@ describe("arrayDiff", () => {
   test("should return an empty array with nothing to be removed", () => {
     expect(arrayDiff([],[1])).toEqual([]);
   })
+
+  test("should return the original array when taking out nother", () => {
+    expect(arrayDiff([1],[])).toEqual([]);
+  })
 })
