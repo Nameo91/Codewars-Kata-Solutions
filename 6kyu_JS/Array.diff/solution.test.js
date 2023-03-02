a8cd7a4c93a16e110048747ce04ba987ff34e7ee
narae <narae1096@gmail.com>
2023-03-02 17:04:43 +0000

    pass second test

--- 6kyu_JS/Array.diff/solution.test.js
+++ 6kyu_JS/Array.diff/solution.test.js
@@ -4,4 +4,8 @@ describe("arrayDiff", () => {
   test("should return an empty array with two empty arrays", () => {
     expect(arrayDiff([],[])).toEqual([]);
   });
+
+  test("should return an empty array with nothing to be removed", () => {
+    expect(arrayDiff([],[1])).toEqual([]);
+  })
 })
