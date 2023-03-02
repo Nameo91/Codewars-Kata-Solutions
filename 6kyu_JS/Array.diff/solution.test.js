d222ba21a7bf4d02d6128392f29e3f3d37d6286c
narae <narae1096@gmail.com>
2023-03-02 16:59:15 +0000

    first test with empty arrays

---
+++ 6kyu_JS/Array.diff/solution.test.js
@@ -0,0 +1,7 @@
+const arrayDiff = require('./solution');
+
+describe("arrayDiff", () => {
+  test("should return an empty array with two empty arrays", () => {
+    expect(arrayDiff([],[])).toBe([]);
+  });
+})
