15350f2af5b334a150002d4309500b40327ae3ee
narae <narae1096@gmail.com>
2023-03-02 17:19:54 +0000

    add more tests

--- 6kyu_JS/Array.diff/solution.test.js
+++ 6kyu_JS/Array.diff/solution.test.js
@@ -7,13 +7,21 @@ describe("arrayDiff", () => {
 
   test("should return an empty array with nothing to be removed", () => {
     expect(arrayDiff([],[1])).toEqual([]);
-  })
+  });
 
   test("should return the original array when taking out nother", () => {
     expect(arrayDiff([1],[])).toEqual([1]);
-  })
+  });
 
   test("should remove one value in the second array from the first one", () => {
     expect(arrayDiff([1,2],[1])).toEqual([2]);
-  })
+  });
+
+  test("should remove all values", () => {
+    expect(arrayDiff([1,2,2,2,3],[2])).toEqual([1,3]);
+  });
+
+  test("should remove two values in the second array", () => {
+    expect(arrayDiff([1,2,2,2,3],[1,2])).toEqual([3]);
+  });
 })
