5dace71f03cbda9486adf4900e4e5b3a2bbb78f1
narae <narae1096@gmail.com>
2023-03-03 18:22:04 +0000

    pass the second test

--- 6kyu_JS/Bit_Counting/solution.test.js
+++ 6kyu_JS/Bit_Counting/solution.test.js
@@ -1,7 +1,11 @@
 const countBits = require('./solution');
 
 describe("countBits", () => {
-  test("should return 0", () => {
+  test("should return 0 for 0000", () => {
     expect(countBits(0)).toBe(0);
   });
+
+  test("should return 1 for 0001", () => {
+    expect(countBits(1)).toBe(1);
+  })
 })
