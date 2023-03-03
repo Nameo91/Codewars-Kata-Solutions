e6b5b6d6cdfdff0ef5969a368cc7444e130243a3
narae <narae1096@gmail.com>
2023-03-03 18:23:08 +0000

    third test

--- 6kyu_JS/Bit_Counting/solution.test.js
+++ 6kyu_JS/Bit_Counting/solution.test.js
@@ -7,5 +7,9 @@ describe("countBits", () => {
 
   test("should return 1 for 0001", () => {
     expect(countBits(1)).toBe(1);
+  });
+
+  test("should return 1 for 0010", () => {
+    expect(countBits(2)).toBe(1);
   })
 })
