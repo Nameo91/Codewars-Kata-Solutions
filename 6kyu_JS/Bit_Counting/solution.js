f9e3e4d57c56ee6e172dd01bd29beba6f4df1f21
narae <narae1096@gmail.com>
2023-03-03 18:41:53 +0000

    count bits

--- 6kyu_JS/Bit_Counting/solution.js
+++ 6kyu_JS/Bit_Counting/solution.js
@@ -1,5 +1,21 @@
 const countBits = (n) => {
-  return n
+  // convert input to binary number
+  const binary = n.toString(2);
+
+  // seperate in an array
+  const bits = binary.split('');
+
+  // count 1 in the array
+  let count = 0;
+  bits.forEach((bit) => {
+    console.log(bit);
+    if (bit == 1) {
+      count += 1
+    }
+    return count;
+  });
+
+  return count;
 };
 
 module.exports = countBits;
