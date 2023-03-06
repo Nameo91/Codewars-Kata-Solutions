bba01a02b5ab08a5b727ca0341a0fc654c82166c
narae <narae1096@gmail.com>
2023-03-06 11:49:45 +0000

    pass more tests

--- 6kyu_JS/Take_a_ten_minutes_walk/solution.test.js
+++ 6kyu_JS/Take_a_ten_minutes_walk/solution.test.js
@@ -20,4 +20,24 @@ describe("isValidWalk", () => {
   test("should return true when the walk returns to starting point in 10 movemnets", () => {
     expect(isValidWalk(['w','e','w','e','w','e','w','e','w','e'])).toBe(true);
   });
+
+  test("should return false when not returning to starting point in 10 movemnets", () => {
+    expect(isValidWalk(['n','s','n','s','n','s','n','n','n','n'])).toBe(false);
+  });
+
+  test("should return false when not returning to starting point in 10 movemnets", () => {
+    expect(isValidWalk(['w','e','w','e','w','e','w','w','w','w'])).toBe(false);
+  });
+
+  test("should return false when not returning to starting point in 10 movemnets", () => {
+    expect(isValidWalk(['w','e','n','s','w','e','n','s','w','w'])).toBe(false);
+  });
+
+  test("should return true when returning to starting point in 10 movemnets", () => {
+    expect(isValidWalk(['w','e','n','s','w','e','n','s','w','e'])).toBe(true);
+  });
+
+  // test("should return false for more than 10 movemnets", () => {
+  //   expect(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])).toBe(false);
+  // });
 })
