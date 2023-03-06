27ad388aa3e483bd98f950e6f9d03d3558f3ae39
narae <narae1096@gmail.com>
2023-03-06 11:39:52 +0000

    develop function

--- 6kyu_JS/Take_a_ten_minutes_walk/solution.js
+++ 6kyu_JS/Take_a_ten_minutes_walk/solution.js
@@ -1,5 +1,28 @@
 isValidWalk = (walk) => {
-  return !walk;   
+  //counter of each letter
+  let n = 0;
+  let s = 0;
+  let w = 0;
+  let e = 0;
+
+  // iterate input array and icrement each counter
+  walk.forEach(e => {
+    if(e === 'n') {
+      n++;;
+    } else if(e === 's') {
+      s++;
+    } else if(e === 'w') {
+      w++;
+    } else {
+      e++;
+    }
+  })
+
+  if(n - s === 0 && w - e === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isValidWalk;
