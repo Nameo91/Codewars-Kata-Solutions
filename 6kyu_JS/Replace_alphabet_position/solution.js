526105eca3d60c9eadac3ff8c8131a3515e3b612
narae <narae1096@gmail.com>
2023-03-07 20:14:39 +0000

    return number fron single letter string

--- 6kyu_JS/Replace_alphabet_position/solution.js
+++ 6kyu_JS/Replace_alphabet_position/solution.js
@@ -1,5 +1,13 @@
 alphabetPosition = (text) => {
-  return text
+  for (let i = 0; i < text.length; i++) {
+    if(text[i] === "a") {
+      return "1";
+    } else if(text[i] === "b") {
+      return "2";
+    } else {
+      return "3";
+    }
+  }
 }
 
 module.exports = alphabetPosition;
--- 6kyu_JS/Replace_alphabet_position/solution.test.js
+++ 6kyu_JS/Replace_alphabet_position/solution.test.js
@@ -1,7 +1,15 @@
 const alphabetPosition = require('./solution')
 
 describe("alphbetPosition", () => {
-  test("should return 1 with a", () => {
+  test("should return 1 with a string with single letter a", () => {
     expect(alphabetPosition("a")).toBe("1");
+  });
+
+  test("should return 2 with a string with single letter b", () => {
+    expect(alphabetPosition("b")).toBe("2");
+  });
+
+  test("should return 2 with a string with single letter c", () => {
+    expect(alphabetPosition("c")).toBe("3");
   });
 })
