6b3c1a23ef87d530455fafafc923603ef528605f
narae <narae1096@gmail.com>
2023-03-09 18:52:51 +0000

    more test carried out

--- 6kyu_JS/Counting_Duplicates/solution.test.js
+++ 6kyu_JS/Counting_Duplicates/solution.test.js
@@ -28,4 +28,12 @@ describe("duplicateCount", () => {
   test("should return 2 as it is case insensitive", () => {
     expect(duplicateCount("Indivisibilities")).toBe(2);
   });
+
+  test("should return 2 as it is case insensitive", () => {
+    expect(duplicateCount("aA11")).toBe(2);
+  });
+
+  test("should return 2 as it is case insensitive", () => {
+    expect(duplicateCount("ABBA")).toBe(2);
+  });
 })
