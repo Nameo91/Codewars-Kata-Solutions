812baa6b5a82b4de235a1afaf3ea5c1db47ebc06
narae <narae1096@gmail.com>
2023-04-14 16:15:46 +0100

    refactor the function

--- 6kyu_JS/Unique_In_Order/solution.js
+++ 6kyu_JS/Unique_In_Order/solution.js
@@ -1,10 +1,11 @@
 const uniqueInOrder = (iterable) => {
+  // convert all the inputs into an array
   if(typeof(iterable) === 'string') {
     iterable = iterable.split('');
   }
 
   let uniqueArray = iterable.filter((item, i) => 
-    i === 0 || item !== iterable[i-1]  //Compare the current value with the previous value
+    item !== iterable[i-1]  //Compare the current value with the previous value
   ); 
   return uniqueArray;
 }
