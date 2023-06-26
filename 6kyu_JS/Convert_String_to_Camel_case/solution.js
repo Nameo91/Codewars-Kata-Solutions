eba17273fc0664838812492ef46032844e84db38
Narae Kim <naraekim@192.168.1.131>
2023-06-26 17:20:42 +0100

    refactor the function

--- 6kyu_JS/Convert_String_to_Camel_case/solution.js
+++ 6kyu_JS/Convert_String_to_Camel_case/solution.js
@@ -2,11 +2,7 @@ toCamelCase = (str) => {
   let newStr = str.replace(/[_-]/gi, ' ').split(' ');
   
   const newWords = newStr.map((word, index) => {
-    if(index == 0) {
-      return word
-    } else {
-      return word.charAt(0).toUpperCase() + word.slice(1)
-    }
+    return index == 0 ? word : word.charAt(0).toUpperCase() + word.slice(1);
   })
   return newWords.join('');
 }
