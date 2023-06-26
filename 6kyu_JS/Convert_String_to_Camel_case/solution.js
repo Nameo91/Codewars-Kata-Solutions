c2615e0adba30060191d47a986d6d8d262741703
Narae Kim <naraekim@192.168.1.131>
2023-06-26 17:18:48 +0100

    return the camelcase string

--- 6kyu_JS/Convert_String_to_Camel_case/solution.js
+++ 6kyu_JS/Convert_String_to_Camel_case/solution.js
@@ -1,5 +1,14 @@
 toCamelCase = (str) => {
-  return str.replace(/[^\w\s]/gi, '')
+  let newStr = str.replace(/[_-]/gi, ' ').split(' ');
+  
+  const newWords = newStr.map((word, index) => {
+    if(index == 0) {
+      return word
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }
+  })
+  return newWords.join('');
 }
 
 module.exports = toCamelCase;
--- 6kyu_JS/Convert_String_to_Camel_case/solution.test.js
+++ 6kyu_JS/Convert_String_to_Camel_case/solution.test.js
@@ -7,21 +7,35 @@ describe("toCamelCase", () => {
 
   test("returns the uppercase letter", () => {
     expect(toCamelCase('A')).toEqual('A');
-  })
+  });
 
   test("eliminates dash or underscore", () => {
     expect(toCamelCase('A-B-C')).toEqual('ABC');
-  })
+  });
 
-  test("combines all the words without any special characters", () => {
-    expect(toCamelCase('The_Stealth_Warrior'))
-  })
+  test("combines all the words", () => {
+    expect(toCamelCase('The_Stealth_Warrior')).toEqual('TheStealthWarrior');
+  });
 
-  test("combines all the words without any special characters", () => {
-    expect(toCamelCase('The_Stealth-Warrior'))
-  })
+  test("combines all the words", () => {
+    expect(toCamelCase('The_Stealth-Warrior')).toEqual('TheStealthWarrior');
+  });
 
-  test("combines all the words without any special characters", () => {
-    expect(toCamelCase('The-Stealth-Warrior'))
-  })
+  test("combines all the words", () => {
+    expect(toCamelCase('The-Stealth-Warrior')).toEqual('TheStealthWarrior');
+  });
+  
+  test("combines all the words", () => {
+    expect(toCamelCase('The-Stealth-Warrior')).toEqual('TheStealthWarrior');
+  });
+
+
+  test("converts the first letter of each word into uppercase", () => {
+    expect(toCamelCase('The-stealth-warrior')).toEqual('TheStealthWarrior');
+  });
+
+
+  test("combines all the words keeping the first letter", () => {
+    expect(toCamelCase('the-Stealth-Warrior')).toEqual('theStealthWarrior');
+  });
 })
