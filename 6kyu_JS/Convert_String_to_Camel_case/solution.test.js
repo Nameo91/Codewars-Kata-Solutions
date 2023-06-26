8c9a8b5ecdcff748b4b06f747300340c97b01485
Narae Kim <naraekim@192.168.1.131>
2023-06-26 15:54:19 +0100

    set the first test

--- 6kyu_JS/Convert_String_to_Camel_case/solution.test.js
+++ 6kyu_JS/Convert_String_to_Camel_case/solution.test.js
@@ -1,1 +1,7 @@
 const toCamelCase = require('./solution');
+
+describe("toCamelCase", () => {
+  test("returns the original lowercase letter", () => {
+    expect(toCamelCase('a')).toEqual('a');
+  });
+})
