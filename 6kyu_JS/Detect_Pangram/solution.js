fa27ef4ec8c11b3e9dfe2f182ed0145e714875e9
Narae Kim <naraekim@192.168.1.131>
2023-06-27 11:18:56 +0100

    reverse to original approach

--- 6kyu_JS/Detect_Pangram/solution.js
+++ 6kyu_JS/Detect_Pangram/solution.js
@@ -1,12 +1,11 @@
 isPangram = (string) => {
-  return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
-  // let allMatches = string.toLowerCase().match(/[a-z]/g || []);
+  let allMatches = string.toLowerCase().match(/[a-z]/g || []);
   
-  // let uniqueMatches = allMatches.filter((letter, index) => {
-  //   return allMatches.indexOf(letter) == index
-  // });
+  let uniqueMatches = allMatches.filter((letter, index) => {
+    return allMatches.indexOf(letter) == index
+  });
 
-  // return uniqueMatches.length === 26;
+  return uniqueMatches.length === 26;
 }
 
 module.exports = isPangram;
