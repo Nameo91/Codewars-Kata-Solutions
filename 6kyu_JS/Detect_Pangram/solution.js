ef9cd2d4fd5fb19f32d89b4fe6995616eecec483
Narae Kim <naraekim@192.168.1.131>
2023-06-27 11:17:26 +0100

    refactor the function

--- 6kyu_JS/Detect_Pangram/solution.js
+++ 6kyu_JS/Detect_Pangram/solution.js
@@ -1,5 +1,12 @@
 isPangram = (string) => {
+  return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+  // let allMatches = string.toLowerCase().match(/[a-z]/g || []);
+  
+  // let uniqueMatches = allMatches.filter((letter, index) => {
+  //   return allMatches.indexOf(letter) == index
+  // });
 
+  // return uniqueMatches.length === 26;
 }
 
 module.exports = isPangram;
--- 6kyu_JS/Detect_Pangram/solution.test.js
+++ 6kyu_JS/Detect_Pangram/solution.test.js
@@ -1,5 +1,17 @@
 const isPangram = require('./solution');
 
 describe('isPangram', () => {
-  
+  test('returns false with one letter', () => {
+    expect(isPangram('a')).toBeFalsy();
+  });
+  test('returns true when it is a pangram', () => {
+    expect(isPangram('abcdefghijklmnopqrstuvwxyz')).toBeTruthy();
+  });
+
+  test('returns true with some uppercases', () => {
+    expect(isPangram("The quick brown fox jumps over the lazy dog")).toBeTruthy();
+  });
+  test('ignores puncuation or numbers', () => {
+    expect(isPangram("The quick brown fox jumps over the lazy dog.")).toBeTruthy();
+  })
 })
